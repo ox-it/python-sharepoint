2cb2b0a11c169a58261649ac5cc7b04da230d0dd
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2012-11-17 22:13:25 +0000

    Fixed namespace prefixes to restore sps to sp.

--- sharepoint/xml.py
+++ sharepoint/xml.py
@@ -5,23 +5,23 @@ namespaces = {
     'wsdl': 'http://schemas.xmlsoap.org/wsdl/',
     'soap': 'http://schemas.xmlsoap.org/soap/envelope/',
     't': 'http://schemas.microsoft.com/exchange/services/2006/types',
-    'sps': 'http://schemas.microsoft.com/sharepoint/soap/',
+    'sp': 'http://schemas.microsoft.com/sharepoint/soap/',
     'spd': 'http://schemas.microsoft.com/sharepoint/soap/directory/',
     'rs': 'urn:schemas-microsoft-com:rowset',
     'ups': 'http://microsoft.com/webservices/SharePointPortalServer/UserProfileService/GetUserProfileByIndex',
     'd': 'http://schemas.microsoft.com/ado/2007/08/dataservices',
     'm': 'http://schemas.microsoft.com/ado/2007/08/dataservices/metadata',
     'z': '#RowsetSchema', # Yes, really.
-    'sp': 'https://github.com/ox-it/python-sharepoint/',
+    'sharepoint': 'https://github.com/ox-it/python-sharepoint/', # Ours
 }
 
 SOAP = builder.ElementMaker(namespace=namespaces['soap'], nsmap=namespaces)
 T = builder.ElementMaker(namespace=namespaces['t'], nsmap=namespaces)
-SP = builder.ElementMaker(namespace=namespaces['sps'], nsmap=namespaces)
+SP = builder.ElementMaker(namespace=namespaces['sp'], nsmap=namespaces)
 SPD = builder.ElementMaker(namespace=namespaces['spd'], nsmap=namespaces)
 UPS = builder.ElementMaker(namespace=namespaces['ups'], nsmap=namespaces)
 
-OUT = builder.ElementMaker(namespace=namespaces['sp'], nsmap=namespaces)
+OUT = builder.ElementMaker(namespace=namespaces['sharepoint'], nsmap=namespaces)
 
 def soap_body(*args, **kwargs):
     return SOAP.Envelope(SOAP.Body(*args, **kwargs))

18d4877314c5fb70b73615bf8f4382654afd467b
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-06-27 13:33:00 +0100

    Bump to version 0.4.1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-__version__ = '0.4'
+__version__ = '0.4.1'
 
 packages = ['sharepoint',
             'sharepoint.lists']
--- sharepoint/__init__.py
+++ sharepoint/__init__.py
@@ -1,4 +1,5 @@
 from .auth import basic_auth_opener
 from .site import SharePointSite
 
-__version__ = '0.4'
+__version__ = '0.4.1'
+
